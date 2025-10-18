5e8c7bbaafd6452df4c0497d047a1a1ff05150a6
wzp <1239263644@qq.com>
2025-10-19 04:34:09 +0800

    增加HttpServer，用于实现连接层的配置  。APP，用于应用层的设置

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,4 +1,18 @@
-use actix_web::{web， App， HttpServer， Responder,HttpRequest}
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpRequest};
+
+ #[tokio::main]
+ async fn main() -> std::io::Result<()> {
+   HttpServer::new(|| {
+    App::new()
+    .route("/", web::get().to(greet))
+   })
+   .bind("127.0.0.1:8080")
+   .unwrap()
+   .run()
+   .await;
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello, {}!", name)
 }
