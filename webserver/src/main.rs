d1192bedae27e020e215803d8181d236fa0ab4bd
wzp <1239263644@qq.com>
2025-10-19 13:40:17 +0800

    增加新的路由，健康监测

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -5,6 +5,8 @@ use actix_web::{web, App, HttpServer, Responder, HttpRequest};
    HttpServer::new(|| {
     App::new()
     .route("/", web::get().to(greet))
+    .route("/{name}", web::get().to(greet))
+    .route("/health", web::get().to(health_check))
    })
    .bind("127.0.0.1:8080")
    .unwrap()
@@ -15,4 +17,9 @@ use actix_web::{web, App, HttpServer, Responder, HttpRequest};
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello, {}!", name)
+}
+
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+HttpResponse::Ok()
 }
