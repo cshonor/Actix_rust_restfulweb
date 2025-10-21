520891246c7a55b22775ad1d2e6ba4fd9abe0662
wzp <1239263644@qq.com>
2025-10-21 19:32:12 +0800

    使用actix_web::dev::Server 修改 run()方法，删除async  删除await 用 let server变量来接受 用OK方法来包装server

--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
+use actix_web::dev::Server;
 
 pub async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,23 +11,14 @@ pub async fn health_check(req: HttpRequest) -> impl Responder {
 HttpResponse::Ok()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+pub  fn run() -> Result<Server, std::io::Error> {
+   let server = HttpServer::new(|| {
        
         App::new()
         .route("/", web::get().to(greet))
         .route("/{name}", web::get().to(greet))
         .route("/health", web::get().to(health_check))})
-    .bind("127.0.0.1:8080")
-    .unwrap()
-    .run()
-    .await
-}
-
-pub  async fn create_app() -> App {
-       
-    App::new()
-    .route("/", web::get().to(greet))
-    .route("/{name}", web::get().to(greet))
-    .route("/health", web::get().to(health_check))
+    .bind("127.0.0.1:8080")?
+    .run();
+    Ok(server)
 }
