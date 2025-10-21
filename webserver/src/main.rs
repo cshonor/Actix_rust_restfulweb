8cd3d8ed642b7181e63a0d67018140c2349627f5
wzp <1239263644@qq.com>
2025-10-21 19:46:23 +0800

    run的成功的时候，返回类型是server，Server 类型的好处 非阻塞: Server 对象可以在不阻塞的情况下创建 控制权: 调用者可以决定何时启动服务器 修改 main方法

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,25 +1,9 @@
 use actix_web::{web, App, HttpServer, Responder, HttpRequest};
-
+use webserver::run;
  #[tokio::main]
  async fn main() -> std::io::Result<()> {
-   HttpServer::new(|| {
-    App::new()
-    .route("/", web::get().to(greet))
-    .route("/{name}", web::get().to(greet))
-    .route("/health", web::get().to(health_check))
-   })
-   .bind("127.0.0.1:8080")
-   .unwrap()
-   .run()
-   .await;
+
+   run().await?;
+   
 }
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello, {}!", name)
-}
-
-
-async fn health_check(req: HttpRequest) -> impl Responder {
-HttpResponse::Ok()
-}
