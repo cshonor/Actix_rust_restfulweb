7650a6ded9b44014a6ea4392e39ae38c81f842a1
wzp <1239263644@qq.com>
2025-10-21 21:42:08 +0800

    实现用std::net::TcpListener and Httpserver 的listen 绑定ip+端口。用127.0.0.1.：0特殊性  ，  127.0.0.1:0: 让操作系统自动分配一个可用的端口 ，TcpListener 的 local_addr 会返回实际使用的ip+port

--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
 pub async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -8,17 +9,17 @@ pub async fn greet(req: HttpRequest) -> impl Responder {
 
 
 pub async fn health_check(req: HttpRequest) -> impl Responder {
-HttpResponse::Ok()
+    HttpResponse::Ok().finish()
 }
 
-pub  fn run() -> Result<Server, std::io::Error> {
+pub  fn run(addr: &str) -> Result<Server, std::io::Error> {
    let server = HttpServer::new(|| {
        
         App::new()
         .route("/", web::get().to(greet))
         .route("/{name}", web::get().to(greet))
         .route("/health", web::get().to(health_check))})
-    .bind("127.0.0.1:8080")?
+    .bind(addr)?
     .run();
     Ok(server)
 }
