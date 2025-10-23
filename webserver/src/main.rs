9892138022a0d118781120761653934456a42f54
wzp <1239263644@qq.com>
2025-10-24 04:58:19 +0800

    修改run后，随后修改main方法

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,9 +1,15 @@
 use actix_web::{web, App, HttpServer, Responder, HttpRequest};
-use webserver::startup::run;
- #[tokio::main]
- async fn main() -> std::io::Result<()> {
+use crate::startup::run;
+use crate::configuration::get_configuration;
+use sqlx::{PgConnection, Connection};
+use std::net::TcpListener;
 
-   run().await?;
+ #[tokio::main]
+async fn main() -> std::io::Result<()> {
+let settings=get_configuration().expect("Failed to get configuration");
+let connection=PgConnection::connect(&settings.database.connection_string()).await.expect("Failed to connect to Postgres");
+let listener=TcpListener::bind(format!("{}:{}", settings.application.host, settings.application.port)).expect("Failed to bind port");
+   run(listener, connection).await?;
    Ok(())
 }
 
