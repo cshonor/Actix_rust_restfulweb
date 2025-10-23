73928a0954a87a1ce330dc9732f24bf9110330cb
wzp <1239263644@qq.com>
2025-10-23 08:32:14 +0800

    修改lib.rs 和 mod 用mod.rs  来声明 模块                                                                       ----模块访问规则

--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -1,16 +1,3 @@
-use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
-use actix_web::dev::Server;
-use std::net::TcpListener;
-
-pub  fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-   let server = HttpServer::new(|| {
-       
-        App::new()
-        .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))
-        .route("/health", web::get().to(health_check))})
-        .route("/subscribe", web::post().to(subscribe))
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
+pub mod startup;        
+pub mod routes;
+pub mod configuration;
---
+++ webserver/startup.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+use webserver::configuration::get_configuration;
+pub  fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+
+    let settings = get_configuration().expect("Failed to load configuration");
+    let address = format!("127.0.0.1:{}", settings.application_port);
+    
+    let server = HttpServer::new(|| {
+        
+         App::new()
+         .route("/", web::get().to(greet))
+         .route("/{name}", web::get().to(greet))
+         .route("/health", web::get().to(health_check))})
+         .route("/subscribe", web::post().to(subscribe))
+     .listen(listener)?
+     .run();
+     Ok(server)
+ }
