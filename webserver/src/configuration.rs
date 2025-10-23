1e80bad6292b5f292035f616f8fbc916343a678b
wzp <1239263644@qq.com>
2025-10-24 02:12:43 +0800

    sqlx 的 dotenv 特性会在编译期自动加载 .env 文件，使 DATABASE_URL 对编译期检查可见

--- webserver/src/configuration.rs
+++ webserver/src/configuration.rs
@@ -1,7 +1,11 @@
+use config::{Config, ConfigError, File, Environment};
+use serde::Deserialize;
+
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
@@ -12,6 +16,13 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
 }
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let  settings = config::Config::builder()
    .add_source(config::File::new("configuration.yaml", config::FileFormat::Yaml))
@@ -19,6 +30,8 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
    .try_deserialize::<Settings>()?;
    Ok(settings)
 }
+
+
 impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!("postgres://{}:{}@{}:{}/{}", self.username, self.password, self.host, self.port, self.database_name)
