c004befe409c68371ce78cff739c9fdc1491b35b
wzp <1239263644@qq.com>
2025-10-23 22:00:20 +0800

    增加了返回数据库的设置和测试sqlx的pgconnection， 测试sqlx的query！宏

--- webserver/src/configuration.rs
+++ webserver/src/configuration.rs
@@ -18,3 +18,8 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
    .build()?
    .try_deserialize::<Settings>()
 }
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!("postgres://{}:{}@{}:{}/{}", self.username, self.password, self.host, self.port, self.database_name)
+    }
+}   
--- webserver/src/routes/health.rs
+++ webserver/src/routes/health.rs
@@ -1,4 +1,5 @@
+use actix_web::{web, HttpRequest, HttpResponse, Responder};
 pub async fn health_check(req: HttpRequest) -> impl Responder {
-    
+    println!("health_check");
     HttpResponse::Ok().finish()
 }
