51c65072939b3f81028588e69d6e1d5391b1ba7d
wzp <1239263644@qq.com>
2025-10-25 02:36:28 +0800

    修改subscribe方法 ，参数加_前缀表示表示未使用，仅占位 将println！的内容用info替换

--- webserver/src/routes/subscribe.rs
+++ webserver/src/routes/subscribe.rs
@@ -8,10 +8,19 @@ pub struct Subscriber {
     pub name: String,
     pub email: String,
 }
-pub async fn subscribe(req: HttpRequest, form: web::Form<Subscriber>,db_pool: web::Data<PgPool>) -> impl Responder {
+pub async fn subscribe(_req: HttpRequest, form: web::Form<Subscriber>,db_pool: web::Data<PgPool>) -> impl Responder {
     log::info!("request body: {:?}", form); 
-    let result = sqlx::query!("INSERT INTO subscriptions (id,email, name, subscribed_at) VALUES ($1, $2, $3, $4)"
+    match sqlx::query!("INSERT INTO subscriptions (id,email, name, subscribed_at) VALUES ($1, $2, $3, $4)"
     , Uuid::new_v4(), form.email, form.name, chrono::Utc::now())
-    .execute(db_pool.get_ref()).await?;
-    HttpResponse::Ok().finish()
+    .execute(db_pool.get_ref()).await
+    {
+        Ok(_)=>{
+            log::info!("Subscription successful"); 
+            HttpResponse::Ok().finish()
+        },
+        Err(e)=>{
+            log::error!("Failed to subscribe: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
