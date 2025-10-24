cbd2aeeefa4b284d2dec84a8dcd3397ea6ae1ba3
wzp <1239263644@qq.com>
2025-10-24 21:47:21 +0800

    actix_web::middleware::Logger 中间件（用于生成 HTTP 请求相关日志）。log ：日志门面，定义统一的日志接口（如 info! 宏、日志级别），是 actix-web 中间件和 env_logger 通信的 桥梁。env_logger ：底层日志实现，负责读取 RUST_LOG 环境变量配置（如日志级别、输出格式），并将日志打印到控制台。

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -3,9 +3,10 @@ use crate::startup::run;
 use crate::configuration::get_configuration;
 use sqlx::{PgPool};
 use std::net::TcpListener;
-
+use env_logger;
  #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
     let settings=get_configuration().expect("Failed to get configuration");
     let db_pool=PgPool::connect(&settings.database.connection_string()).await.expect("Failed to connect to Postgres");
     let listener=TcpListener::bind(format!("{}:{}", settings.application.host, settings.application.port)).expect("Failed to bind port");
--- webserver/src/routes/subscribe.rs
+++ webserver/src/routes/subscribe.rs
@@ -9,6 +9,7 @@ pub struct Subscriber {
     pub email: String,
 }
 pub async fn subscribe(req: HttpRequest, form: web::Form<Subscriber>,db_pool: web::Data<PgPool>) -> impl Responder {
+    log::info!("request body: {:?}", form); 
     let result = sqlx::query!("INSERT INTO subscriptions (id,email, name, subscribed_at) VALUES ($1, $2, $3, $4)"
     , Uuid::new_v4(), form.email, form.name, chrono::Utc::now())
     .execute(db_pool.get_ref()).await?;
