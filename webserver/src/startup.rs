4284bbbe91f9970a84b5dd3faddb0089784f19ec
wzp <1239263644@qq.com>
2025-10-25 19:38:28 +0800

    给数据加span   用sqlx.instrument(db_span)：这部分代码通常与 tracing 相关，instrument方法用于将异步操作与指定的 tracing span 关联起来，便于对数据库操作进行性能分析和调试等，db_span是一个预先定义好的 span。

--- webserver/src/startup.rs
+++ webserver/src/startup.rs
@@ -5,9 +5,21 @@ use std::net::TcpListener;
 use crate::configuration::get_configuration;
 use crate::routes::{greet, health_check, subscribe};
 use sqlx::PgPool;
+use tracing_subscriber::fmt::Subscriber;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{Registry, EnvFilter};
 
+fn get_subscriber(name: String, env_filter: String) -> Subscriber {
+   
+    let env_filter = EnvFilter::new(env_filter);
+   let registry = Registry::default().with(env_filter).with(formatting_layer);
+    let formatting_layer = fmt::layer().with_target(true).with_line_number(true).with_file(true);
+    Subscriber::builder().with_subscriber(env_filter).with_ansi(true).with_timer(ChronoTimer::new()).with_file(true).with_line_number(true).with_target(true).finish()
+}   
 pub  fn run(listener: TcpListener, db_pool:PgPool) -> Result<Server, std::io::Error> {
-
+let subscriber = get_subscriber("webserver".into(), "info".into(), std::io::stdout);
+let _ = subscriber.init();
         let settings = get_configuration().expect("Failed to load configuration");
         let address = format!("127.0.0.1:{}", settings.application_port);
         let db_pool = web::Data::new(db_pool);
