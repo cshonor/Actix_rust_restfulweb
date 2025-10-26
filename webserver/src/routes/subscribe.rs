5bf07ed370bc795795d39485cf05e6a85ce2ff9f
wzp <1239263644@qq.com>
2025-10-27 04:09:53 +0800

    新增 tracing::instrument 属性宏    #[tracing::instrument]是 Rust 语言中tracing crate 里的一个属性宏，用于为函数或异步函数添加追踪跨度（span），方便对程序运行过程进行跟踪和调试  1.每次调用被#[tracing::instrument]修饰的函数时，都会创建一个以函数名命名的跨度。2使用name参数可覆盖默认的跨度名称，例如#[instrument(name = my_name)]，会将跨度名称设置为my_name。3.忽略参数记录：若不想记录某个函数参数，可通过skip参数实现。如#[instrument(skip(non_debug))]，表示不记录non_debug参数。4. 添加额外字段：借助fields参数能为生成的跨度添加额外的键值对字段，如#[instrument(fields(foo = bar, id = 1, show = true))]，会为跨度添加foo、id和show字段。

--- webserver/src/routes/subscribe.rs
+++ webserver/src/routes/subscribe.rs
@@ -10,31 +10,30 @@ pub struct Subscriber {
     pub name: String,
     pub email: String,
 }
+#[tracing::instrument(
+    name = "Adding a new subscriber", 
+    skip(form, db_pool),
+    fields(request_id = %Uuid::new_v4(),email = %form.email,name = %form.name))]
 pub async fn subscribe(_req: HttpRequest, form: web::Form<Subscriber>,db_pool: web::Data<PgPool>) -> impl Responder {
     
-    let request_id = Uuid::new_v4();
-    let span = tracing::info_span!("subscribe",
-        request_id = %request_id,
-        email = %form.email,
-        name = %form.name
-    );
-
-    let _enter = span.enter();
-    tracing::info!(" request_id: {} request body: {:?}", request_id, form);
-
-
-    let db_span = tracing::info_span!("subscribe_db");
-    match sqlx::query!("INSERT INTO subscriptions (id,email, name, subscribed_at) VALUES ($1, $2, $3, $4)"
-    , request_id, form.email, form.name, chrono::Utc::now())
-    .execute(db_pool.get_ref()).instrument(db_span).await
+    match insert_subscriber(&db_pool, &form).await
     {
         Ok(_)=>{
-            tracing::info!(request_id = %request_id, "Subscription successful"); 
             HttpResponse::Ok().finish()
         },
-        Err(e)=>{
-            tracing::error!(request_id = %request_id, "Failed to subscribe: {}", e);
+        Err(_)=>{
             HttpResponse::InternalServerError().finish()
         }
     }
+}
+#[tracing::instrument(name = "Inserting a new subscriber", skip(form, db_pool),fields(request_id = %Uuid::new_v4(),email = %form.email,name = %form.name))]
+
+pub async fn insert_subscriber(db_pool: &PgPool, form:&Subscriber) -> Result<(), sqlx::Error> {
+    sqlx::query!("INSERT INTO subscriptions (id,email, name, subscribed_at) VALUES ($1, $2, $3, $4)"
+    , Uuid::new_v4(), email, name, chrono::Utc::now())
+    .execute(db_pool.get_ref()).await
+    .map_err(|e| {
+        tracing::error!("Failed to insert subscriber: {}", e);
+        e
+    })
 }
