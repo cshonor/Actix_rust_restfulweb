249e8f843f827f823b85b05bbdd7cc00133da2b1
wzp <1239263644@qq.com>
2025-10-27 00:41:03 +0800

    用std::io::sink std::io::sink 函数用于创建一个 std::io::Sink 结构体的实例，该实例是一个特殊的写入器，它会成功地消耗所有写入的数据，但实际上不会对数据进行任何操作，只是简单地将数据丢弃

--- webserver/src/routes/telemetry.rs
+++ webserver/src/routes/telemetry.rs
@@ -1,10 +1,12 @@
 use tracing::Subscriber;
-use tracing_subscriber::{EnvFilter, Registry};
+use tracing_subscriber::{EnvFilter, Registry,fmt::MakeWriter};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 
-pub fn get_subscriber(name: String, env_filter: EnvFilter) -> impl Subscriber + Send + Sync {
+pub fn get_subscriber(name: String, env_filter: EnvFilter, sink:Sink) -> impl Subscriber + Send + Sync 
+      where Sink: for<'a> MakeWriter<'a>+Send+Sync+ 'static
+{
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
     Registry::default().with(env_filter).with(JsonStorageLayer).with(formatting_layer)
 }
 
