3a2886423a43f1a66631cea56fe37904af43d33a
wzp <1239263644@qq.com>
2025-10-28 06:14:39 +0800

    修复DatabaseSettings后 返回的是加密信息 要使用url 要解密

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -4,7 +4,7 @@ use sqlx::{PgPool};
 use std::net::TcpListener;
 use tracing_subscriber::{EnvFilter};
 use webserver::routes::telemetry::{get_subscriber, init_subscriber};
-
+use secrecy::ExposeSecret;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
 
@@ -12,7 +12,7 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let settings=get_configuration().expect("Failed to get configuration");
-    let db_pool=PgPool::connect(&settings.database.connection_string()).await.expect("Failed to connect to Postgres");
+    let db_pool=PgPool::connect(&settings.database.connection_string().expose_secret()).await.expect("Failed to connect to Postgres");
     let listener=TcpListener::bind(format!("{}:{}", settings.application.host, settings.application.port)).expect("Failed to bind port");
     run(listener, db_pool)?.await?;
     Ok(())
