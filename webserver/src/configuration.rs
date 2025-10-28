a3c042791d81a6385d16f6b655308f01d5d3d615
wzp <1239263644@qq.com>
2025-10-28 22:40:30 +0800

    支持多环境（本地、生产等）的配置区分分为基础配置文件（存储本地和生产环境共享的配置，如数据库名称）和环境特定配置文件（存储各环境定制的配置，如主机地址）。

--- webserver/src/configuration.rs
+++ webserver/src/configuration.rs
@@ -1,4 +1,4 @@
-use config::{Config, ConfigError, File, Environment};
+use config::{Config, ConfigError, File, Environment, TryInto};
 use serde::Deserialize;
 use secrecy::{Secret, ExposeSecret};
 #[derive(serde::Deserialize)]
@@ -22,12 +22,48 @@ pub struct ApplicationSettings {
     pub host: String,
 }
 
+#[derive(Debug, Deserialize)]
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Environment::Local),
+            "production" => Ok(Environment::Production),
+            other => Err(format!("{} is not a valid environment", other)),
+        }
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let  settings = config::Config::builder()
-   .add_source(config::File::new("configuration.yaml", config::FileFormat::Yaml))
-   .build()?
-   .try_deserialize::<Settings>()?;
-   Ok(settings)
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+    .unwrap_or_else(|_| "local".into())
+    .try_into()
+    .expect("Failed to parse APP_ENVIRONMENT");
+
+    let environment_filename = format!("{}.yaml", environment.as_str());
+    let settings = config::Config::builder()
+    .add_source(config::File::from(configuration_directory.join(&environment_filename)))
+    .add_source(config::File::from(configuration_directory.join("base.yaml")))
+    .build()?
+    .try_deserialize::<Settings>()?;
+    Ok(settings)
 }
 
 
