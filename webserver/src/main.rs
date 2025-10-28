8c054f65dc4592b24faab6f38821eb778be46051
wzp <1239263644@qq.com>
2025-10-29 02:02:59 +0800

    用connect_lazy 这个会在首次使用连接池时候尝试建立连接

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -13,7 +13,7 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let settings=get_configuration().expect("Failed to get configuration");
-    let db_pool=PgPool::connect(&settings.database.connection_string().expose_secret()).await.expect("Failed to connect to Postgres");
+    let db_pool=PgPool::connect_lazy(&settings.database.connection_string().expose_secret()).await.expect("Failed to connect to Postgres");
     let listener=TcpListener::bind(format!("{}:{}", settings.application.host, settings.application.port)).expect("Failed to bind port");
     run(listener, db_pool)?.await?;
     Ok(())
