9122652288805f995603c10fcd567129832775c2
wzp <1239263644@qq.com>
2025-10-31 07:12:02 +0800

    增加一个名为 SubscriberName 的元组结构体 1.假设 SubscriberName 代表邮箱，需要保证格式合法。元组无法限制外部随意赋值非法字符串，但元组结构体可以通过 私有字段 + 验证方法 实现 2.可以像普通结构体一样，为其单独实现方法、关联函数或 trait，扩展性更强。

--- webserver/src/routes/mod.rs
+++ webserver/src/routes/mod.rs
@@ -2,8 +2,10 @@ pub mod subscribe;
 pub mod health;
 pub mod greet;
 pub mod telemetry;
+pub mod domain;
 
 pub use subscribe::*;   
 pub use health::*;   
 pub use greet::*;   
 pub use telemetry::*;
+pub use domain::*;
