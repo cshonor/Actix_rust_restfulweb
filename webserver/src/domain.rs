7a91929ec77e179f2569ced4420b64c7def78c32
wzp <1239263644@qq.com>
2025-10-31 13:55:25 +0800

    用claim依赖做新的断言  在domain模块做单元测试

--- webserver/src/domain.rs
+++ webserver/src/domain.rs
@@ -1,3 +1,4 @@
+use claim::{assert_err, assert_ok};
 pub struct SubscriberName(String);
 
 impl SubscriberName {
@@ -7,9 +8,10 @@ impl SubscriberName {
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = name.chars().any(|c| forbidden_characters.contains(&c));
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            return Err("Subscriber name is not valid".to_string());
+            Err("Subscriber name is not valid".to_string())
+        } else {
+            Ok(Self(name))
         }
-        Ok(Self(name))
     }
 
 
@@ -26,4 +28,42 @@ impl AsRef<str> for SubscriberName {
 pub struct NewSubscriber {
     pub email: SubscriberEmail,
     pub name: SubscriberName,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claim::{assert_err, assert_ok};
+    #[test]
+    fn a_200_OK_result_indicates_success() {
+        let result = SubscriberName::parse("Ursula Le Guin".to_string());
+        assert_ok!(result);
+    }
+    #[test]
+    fn a_400_bad_request_result_indicates_validation_error() {
+        let result = SubscriberName::parse("".to_string());
+        assert_err!(result);
+    }
+    #[test]
+    fn a_400_bad_request_result_indicates_name_too_long() {
+        let result = SubscriberName::parse("a".repeat(257));
+        assert_err!(result);
+    }
+    #[test]
+    fn a_400_bad_request_result_indicates_name_contains_forbidden_characters() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let result = SubscriberName::parse(name.to_string());
+            assert_err!(result);
+        }
+    }
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let result = SubscriberName::parse(" ".to_string());
+        assert_err!(result);
+    }
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let result = SubscriberName::parse("Ursula Le Guin".to_string());
+        assert_ok!(result);
+    }
 }
