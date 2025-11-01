71d2734654981f2dfea9453af6c1b307abfcae5f
wzp <1239263644@qq.com>
2025-11-01 12:24:05 +0800

    增加发送邮箱接口

--- webserver/src/domain/mod.rs
+++ webserver/src/domain/mod.rs
@@ -1,9 +1,10 @@
 pub mod subscriber_name;
 pub mod subscriber_email;
 pub mod new_subscriber;
-
+pub mod email_client;
 
 
 pub use subscriber_name::*;
 pub use subscriber_email::*;
 pub use new_subscriber::*;
+pub use email_client::*;
