e3d592e5d2a1b0a6cdeef6aac870a2f73b0e7d81
wzp <1239263644@qq.com>
2025-11-01 16:23:18 +0800

    修改配置main.rs  因为配置修改了email

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,9 +1,11 @@
 use webserver::startup::run;
 use webserver::configuration::get_configuration;
-use sqlx::{PgPool};
+use sqlx::{PgConnection};
 use std::net::TcpListener;
 use tracing_subscriber::{EnvFilter};
 use webserver::routes::telemetry::{get_subscriber, init_subscriber};
+use crate::domain::email_client::EmailClient;
+use reqwest::Client;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -12,9 +14,16 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let settings=get_configuration().expect("Failed to get configuration");
-    let db_pool=PgPool::connect_lazy_with(settings.database.with_db());
+
+    let db_pool=PgConnectOptions::connect_lazy_with(settings.database.with_db());
+
+    let email_client_settings = settings.email_client.sender().expect("Invalid sender email");
+
+    let email_client = EmailClient::new(email_client_settings, Client::new(), settings.email_client.base_url);
+
     let listener=TcpListener::bind(format!("{}:{}", settings.application.host, settings.application.port)).expect("Failed to bind port");
-    run(listener, db_pool)?.await?;
+   
+    run(listener, db_pool, email_client)?.await?;
     Ok(())
 }
 
